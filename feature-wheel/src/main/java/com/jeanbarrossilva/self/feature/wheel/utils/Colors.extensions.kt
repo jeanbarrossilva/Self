7b2d33e17608bb790f1b0be60adcaa9a982b50be
Jean Silva <jeanbarrossilva@outlook.com>
2023-03-21 19:48:38 -0300

    fix(bug): low contrast between sheet and editing buttons

--- feature-wheel/src/main/java/com/jeanbarrossilva/self/feature/wheel/utils/Colors.extensions.kt
+++ feature-wheel/src/main/java/com/jeanbarrossilva/self/feature/wheel/utils/Colors.extensions.kt
@@ -1,8 +1,9 @@
 package com.jeanbarrossilva.self.feature.wheel.utils
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
 import com.jeanbarrossilva.aurelius.ui.theme.colors.Colors
+import com.jeanbarrossilva.self.feature.wheel.R
 
-internal val Colors.elevated
-    @Composable get() = if (isSystemInDarkTheme()) container.secondary else background
+internal val @receiver:Suppress("Unused") Colors.elevated
+    @Composable get() = colorResource(R.color.elevated)
