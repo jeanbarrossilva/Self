cff9ebb72e27447b0846e1a2879fefe3e2f05187
Jean Silva <jeanbarrossilva@outlook.com>
2023-03-21 16:20:01 -0300

    fix(bug): hardcoded sample wheel

--- feature-wheel/src/main/java/com/jeanbarrossilva/self/feature/wheel/WheelViewModel.kt
+++ feature-wheel/src/main/java/com/jeanbarrossilva/self/feature/wheel/WheelViewModel.kt
@@ -4,13 +4,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.jeanbarrossilva.loadable.utils.loadable
-import com.jeanbarrossilva.self.feature.wheel.domain.FeatureWheel
+import com.jeanbarrossilva.self.feature.wheel.utils.feature
 import com.jeanbarrossilva.self.wheel.core.infra.WheelRepository
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 internal class WheelViewModel(private val repository: WheelRepository) : ViewModel() {
-    val wheel = loadable {
-        load(FeatureWheel.sample)
-    }
+    val wheel = flow { emitAll(repository.fetch()) }.map { it.first().feature() }.loadable()
 
     companion object {
         fun createFactory(repository: WheelRepository): ViewModelProvider.Factory {
