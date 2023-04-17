f35edda48c03cef60834b374465f525b69f0a0c6
Jean Silva <jeanbarrossilva@outlook.com>
2023-04-17 15:46:24 -0300

    fix(bug): repeated chart Y labels

---
+++ feature-wheel/src/main/java/com/jeanbarrossilva/self/feature/wheel/WheelModel.kt
@@ -0,0 +1,13 @@
+package com.jeanbarrossilva.self.feature.wheel
+
+import java.text.NumberFormat
+import java.util.Locale
+
+internal object WheelModel {
+    fun format(attention: Float): String {
+        val locale = Locale.getDefault()
+        return NumberFormat.getPercentInstance(locale).apply { maximumFractionDigits = 1 }.format(
+            attention
+        )
+    }
+}
